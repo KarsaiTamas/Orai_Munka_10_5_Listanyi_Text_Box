c90aa4790a4ffa13e53848f02e1fc6e5fdf7a703
Karsai Tamás <karsai106@gmail.com>
2018-10-06 06:03:21 +0200

    Listanyi_Text_Box

--- Numeric_Gomb/Form1.Designer.cs
+++ Numeric_Gomb/Form1.Designer.cs
@@ -28,12 +28,82 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.Sor_Szam = new System.Windows.Forms.NumericUpDown();
+            this.Sor_Szeret = new System.Windows.Forms.Label();
+            this.Hozzaad = new System.Windows.Forms.Button();
+            this.Ha_Ez_Mindent_Kiir = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.Sor_Szam)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Sor_Szam
+            // 
+            this.Sor_Szam.Location = new System.Drawing.Point(124, 13);
+            this.Sor_Szam.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.Sor_Szam.Name = "Sor_Szam";
+            this.Sor_Szam.Size = new System.Drawing.Size(37, 20);
+            this.Sor_Szam.TabIndex = 0;
+            this.Sor_Szam.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // Sor_Szeret
+            // 
+            this.Sor_Szeret.AutoSize = true;
+            this.Sor_Szeret.Location = new System.Drawing.Point(13, 13);
+            this.Sor_Szeret.Name = "Sor_Szeret";
+            this.Sor_Szeret.Size = new System.Drawing.Size(105, 13);
+            this.Sor_Szeret.TabIndex = 1;
+            this.Sor_Szeret.Text = "Ennyi sort szeretnék:";
+            // 
+            // Hozzaad
+            // 
+            this.Hozzaad.Location = new System.Drawing.Point(167, 8);
+            this.Hozzaad.Name = "Hozzaad";
+            this.Hozzaad.Size = new System.Drawing.Size(75, 23);
+            this.Hozzaad.TabIndex = 2;
+            this.Hozzaad.Text = "Hozzaad";
+            this.Hozzaad.UseVisualStyleBackColor = true;
+            this.Hozzaad.Click += new System.EventHandler(this.Hozzaadas);
+            // 
+            // Ha_Ez_Mindent_Kiir
+            // 
+            this.Ha_Ez_Mindent_Kiir.Location = new System.Drawing.Point(249, 9);
+            this.Ha_Ez_Mindent_Kiir.Name = "Ha_Ez_Mindent_Kiir";
+            this.Ha_Ez_Mindent_Kiir.Size = new System.Drawing.Size(108, 23);
+            this.Ha_Ez_Mindent_Kiir.TabIndex = 3;
+            this.Ha_Ez_Mindent_Kiir.Text = "Naon minden kiíííír";
+            this.Ha_Ez_Mindent_Kiir.UseVisualStyleBackColor = true;
+            this.Ha_Ez_Mindent_Kiir.Click += new System.EventHandler(this.Ha_Ez_Mindent_Kiir_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(731, 345);
+            this.Controls.Add(this.Ha_Ez_Mindent_Kiir);
+            this.Controls.Add(this.Hozzaad);
+            this.Controls.Add(this.Sor_Szeret);
+            this.Controls.Add(this.Sor_Szam);
+            this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.Sor_Szam)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.NumericUpDown Sor_Szam;
+        private System.Windows.Forms.Label Sor_Szeret;
+        private System.Windows.Forms.Button Hozzaad;
+        private System.Windows.Forms.Button Ha_Ez_Mindent_Kiir;
     }
 }
 
--- Numeric_Gomb/Form1.cs
+++ Numeric_Gomb/Form1.cs
@@ -7,14 +7,74 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO;
 
 namespace Numeric_Gomb
 {
     public partial class Form1 : Form
     {
+          List<TextBox> szvegdoboz_lista = new List<TextBox>();
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Hozzaadas(object sender, EventArgs e)
+        {
+            int darab=(int)Sor_Szam.Value;
+            for (int i = 0; i < darab; i++)
+            {
+                TextBox tb = new TextBox();
+                tb.SetBounds(10, 50+i*25, 200, tb.Height);
+                this.Controls.Add(tb);
+                this.szvegdoboz_lista.Add(tb);
+                Button b = new Button();
+                b.SetBounds(230, 50 + i * 25, 75, tb.Height);
+                b.Text = "Push me";
+                this.Controls.Add(b);
+
+                b.MouseUp += (sender1,args)=> {
+                    if (args.Button == MouseButtons.Right) { 
+                    MessageBox.Show("Tartalom:" + tb.Text);}
+                    else if(args.Button == MouseButtons.Left)
+                    {
+                        MessageBox.Show("Balre");
+                    }else if(args.Button == MouseButtons.Middle)
+                    {
+                        MessageBox.Show("Közepes");
+                    }else if(args.Button == MouseButtons.XButton1)
+                    {
+                        MessageBox.Show("Alúvan");
+                    }else if(args.Button == MouseButtons.XButton2)
+                    {
+                        MessageBox.Show("ALJÁBB VaN");
+                    }
+                };
+                Button btorol = new Button();
+                btorol.SetBounds(330, 50 + i * 25, 75, btorol.Height);
+                btorol.Text = "Torles";
+                btorol.Click += (sender1, args) =>
+                {
+                    szvegdoboz_lista.Remove(tb);
+                    this.Controls.Remove(tb);
+                    this.Controls.Remove(b);
+                    this.Controls.Remove(btorol);
+                };
+                this.Controls.Add(btorol);
+            }
+
+
+        }
+
+        private void Ha_Ez_Mindent_Kiir_Click(object sender, EventArgs e)
+        {
+            string szoveg = "";
+            foreach (var item in this.szvegdoboz_lista)
+            {
+                szoveg += item.Text+"\n";
+            }
+            MessageBox.Show(szoveg);
+            File.WriteAllText("kimenet.txt",szoveg);
         }
     }
 }
